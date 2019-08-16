5bb3f2cb4070ba57defbb5057bfc797167faf00f
jacekskowalski <jsk_profil@hotmail.com>
2019-08-16 22:36:41 +0200

    create layout alpha

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import React from 'react';
-import './src/App.css';
+import './index.css';
+import  Calculator  from './Calculator';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="calculator">
+          <div className="calc_input">  <input type="text" name="inputValue" /></div>
+            <div class='row'>
+              <div class='column'>
+                  <div>7 </div>
+              </div>
+              <div class='column'>
+                  <div>8</div>
+              </div>
+              <div class='column'>
+                  <div >9</div>
+              </div>
+              <div class='column'>
+                  <div>x</div>
+              </div>
+          </div>
+          <div class='row'>
+              <div class='column'>
+                  <div >4</div>
+              </div>
+              <div class='column'>
+                  <div>5</div>
+              </div>
+              <div class='column'>
+                  <div>6</div>
+              </div>
+              <div class='column'>
+                  <div>/ </div>
+              </div>
+          </div>
+          <div class='row'>
+              <div class='column'>
+                  <div>1</div>
+              </div>
+              <div class='column'>
+                  <div>2</div>
+              </div>
+              <div class='column'>
+                  <div>3</div>
+              </div>
+              <div class='column'>
+                  <div>-</div>
+              </div>
+          </div>
+          <div class='row'>
+              <div class='column'>
+                  <div>0</div>
+              </div>
+              <div class='column'>
+                  <div>,</div>
+              </div>
+              <div class='column'>
+                  <div>=</div>
+              </div>
+              <div class='column'>
+                  <div>+</div>
+              </div>
+          </div>
+          </div>
+        
   );
 }
 
